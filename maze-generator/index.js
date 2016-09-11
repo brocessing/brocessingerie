8ba167893534086dd0b08db7976e05d4dc707a2e
RNO <arnaud.juracek@gmail.com>
2016-09-11 20:26:35 +0200

    💪 maze-generator

--- maze-generator/index.js
+++ maze-generator/index.js
@@ -1,39 +1,119 @@
-// EXAMPLE
-const maze = require('./../index.js');
+const mazeGenerator = require('./../index.js');
+const canvas = buildCanvas(window.innerWidth - 1, window.innerHeight - 1);
 
+const colors = ["", "#42dc8e", "#2e43eb", "#ffe359", "#96bfed", "#f5ead6", "#f1f3f7", "#e2e6ef"];
 
-let canvas = window.document.createElement('canvas');
-canvas.width = (window.innerWidth < window.innerHeight ? window.innerWidth : window.innerHeight) - 50;
-canvas.height = canvas.width;
-canvas.style.width = canvas.width + 'px';
-canvas.style.height = canvas.height + 'px';
-window.document.body.appendChild(canvas);
+let maze, solver;
 
-maze.generate(50, 50);
-maze.update(canvas);
+const RESOLUTION = 100;
+const GENERATOR_ITERATION_BY_FRAME = 100;
+const SOLVER_ITERATION_BY_FRAME = 5;
 
-window.addEventListener('click', function() {
-  maze.generate(50, 50);
-  maze.update(canvas);
-});
+function init() {
+  let ratio = window.innerWidth / window.innerHeight;
+  let cols = (ratio > 1) ? Math.floor(RESOLUTION*ratio) : RESOLUTION;
+  let rows = (ratio < 1) ? Math.floor(RESOLUTION/ratio) : RESOLUTION;
+
+  maze = new mazeGenerator.Maze(cols, rows);
+  solver = null; // solver is set once the maze is solved, see update();
+}
+
+init();
+loop();
+canvas.addEventListener('click', init);
+
 
 // -------------------------------------------------------------------------
 
-window.requestAnimFrame = (function(){
-  return  window.requestAnimationFrame       ||
-          window.webkitRequestAnimationFrame ||
-          window.mozRequestAnimationFrame    ||
-          function( callback ){
-            window.setTimeout(callback, 1000 / 60);
-          };
-})();
+function buildCanvas(width, height = width){
+  let canvas = window.document.createElement('canvas');
+  canvas.width = width;
+  canvas.height = height;
+  canvas.style.width = canvas.width + 'px';
+  canvas.style.height = canvas.height + 'px';
+  window.document.body.style.margin = '0';
+  window.document.body.style.padding = '0';
+  window.document.body.style.background = 'black';
+  window.document.body.appendChild(canvas);
 
-window.loop = function(){
-  if(maze !== undefined){
-    requestAnimFrame(loop);
-    maze.update(canvas);
+  return canvas;
+}
+
+function loop(){
+  requestAnimationFrame(loop);
+  update(canvas);
+}
+
+function update(canvas) {
+  if (!maze.end) {
+    for (let i = 0; i < GENERATOR_ITERATION_BY_FRAME; i++) maze.step();
+      display(maze, canvas);
+  } else {
+    solver = solver || new mazeGenerator.Solver(maze);
+    if (!solver.done) {
+      for (let i = 0; i < SOLVER_ITERATION_BY_FRAME; i++) {
+        if (solver.step()) {
+          solver.done = true;
+          break;
+        }
+      }
+      display(maze, canvas);
+    }
   }
-};
+}
 
-window.loop();
-window.noLoop = function(){ loop = null; }
+function display(maze, canvas) {
+  let ctx = canvas.getContext('2d');
+  let margin = 10;
+  let w = (canvas.width - margin * 2) / maze.cols;
+  let h = (canvas.height - margin * 2) / maze.rows;
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.beginPath();
+
+  for (let i = 0; i < maze.cells.length; i++) {
+    let cell = maze.cells[i];
+    if (cell === maze.currentCell) {
+      ctx.fillStyle = '#ffe359';
+      ctx.fillRect(margin + cell.x * w, margin + cell.y * h, w, h);
+    } else if (cell.visited) {
+      ctx.fillStyle = '#ffffff';
+      ctx.fillRect(margin + cell.x * w, margin + cell.y * h, w, h);
+      for (let j = 0; j < cell.walls.length; j++) {
+        if (cell.walls[j]) {
+          switch(j) {
+            case 0 :
+            ctx.moveTo(margin + (cell.x + 0) * w, margin + (cell.y + 0) * h );
+            ctx.lineTo(margin + (cell.x + 1) * w, margin + (cell.y + 0) * h );
+            break;
+            case 1 :
+            ctx.moveTo(margin + (cell.x + 1) * w, margin + (cell.y + 0) * h );
+            ctx.lineTo(margin + (cell.x + 1) * w, margin + (cell.y + 1) * h );
+            break;
+            case 2 :
+            ctx.moveTo(margin + (cell.x + 1) * w, margin + (cell.y + 1) * h );
+            ctx.lineTo(margin + (cell.x + 0) * w, margin + (cell.y + 1) * h );
+            break;
+            case 3 :
+            ctx.moveTo(margin + (cell.x + 0) * w, margin + (cell.y + 1) * h );
+            ctx.lineTo(margin + (cell.x + 0) * w, margin + (cell.y + 0) * h );
+            break;
+          }
+        }
+      }
+    }
+    if (cell === maze.start || cell == maze.end) {
+      ctx.fillStyle = '#ff2d5d';
+      ctx.fillRect(margin + cell.x * w, margin + cell.y * h, w, h);
+    } else if (solver) {
+      if (solver.solution && solver.solution.indexOf(cell) > 0) {
+        ctx.fillStyle = 'rgba(66, 220, 142, 0.5)';
+        ctx.fillRect(margin + cell.x * w, margin + cell.y * h, w, h);
+      } else if (cell.flooded) {
+        ctx.fillStyle = 'rgba(46, 67, 235, 0.5)';
+        ctx.fillRect(margin + cell.x * w, margin + cell.y * h, w, h);
+      }
+    }
+  }
+  ctx.stroke();
+}
